a02a7be07bd4d5cf13e6f14a480d8a5caca77790
Patinya Yongyai <patinya.y@ku.th>
2018-05-02 22:35:08 +0700

    add roles dropdown

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,18 +1,123 @@
 <template>
   <div>
-   Admin page
+    <el-tabs type="border-card">
+      <el-tab-pane label="Create">
+        <el-form ref="form" :model="form" label-width="120px">
+          <el-form-item label="E-mail: ">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <!-- <el-form-item label="Activity zone">
+            <el-select v-model="form.region" placeholder="please select your zone">
+              <el-option label="Zone one" value="shanghai"></el-option>
+              <el-option label="Zone two" value="beijing"></el-option>
+            </el-select>
+          </el-form-item> -->
+          <el-form-item label="Password" prop="pass">
+            <el-input type="password" v-model="form.pass" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="Departments">
+            <el-dropdown>
+              <span class="el-dropdown-link">
+                Departments<i class="el-icon-arrow-down el-icon--right"></i>
+              </span>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item>Action 1</el-dropdown-item>
+                <el-dropdown-item>Action 2</el-dropdown-item>
+                <el-dropdown-item>Action 3</el-dropdown-item>
+                <el-dropdown-item>Action 4</el-dropdown-item>
+                <el-dropdown-item>Action 5</el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
+          </el-form-item>
+          <el-form-item label="Roles">
+            <el-radio-group v-model="form.resource">
+              <el-radio label="Sponsor"></el-radio>
+              <el-radio label="Venue"></el-radio>
+            </el-radio-group>
+          </el-form-item>
+
+
+          <el-form-item label="Activity time">
+            <el-col :span="11">
+              <el-date-picker type="date" placeholder="Pick a date" v-model="form.date1" style="width: 100%;"></el-date-picker>
+            </el-col>
+            <el-col class="line" :span="2">-</el-col>
+            <el-col :span="11">
+              <el-time-picker type="fixed-time" placeholder="Pick a time" v-model="form.date2" style="width: 100%;"></el-time-picker>
+            </el-col>
+          </el-form-item>
+          <el-form-item label="Instant delivery">
+            <el-switch on-text="" off-text="" v-model="form.delivery"></el-switch>
+          </el-form-item>
+          <el-form-item label="Activity type">
+            <el-checkbox-group v-model="form.type">
+              <el-checkbox label="Online activities" name="type"></el-checkbox>
+              <el-checkbox label="Promotion activities" name="type"></el-checkbox>
+              <el-checkbox label="Offline activities" name="type"></el-checkbox>
+              <el-checkbox label="Simple brand exposure" name="type"></el-checkbox>
+            </el-checkbox-group>
+          </el-form-item>
+          <el-form-item label="Resources">
+            <el-radio-group v-model="form.resource">
+              <el-radio label="Sponsor"></el-radio>
+              <el-radio label="Venue"></el-radio>
+            </el-radio-group>
+          </el-form-item>
+          <el-form-item label="Activity form">
+            <el-input type="textarea" v-model="form.desc"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit">Create</el-button>
+            <el-button>Cancel</el-button>
+          </el-form-item>
+        </el-form>
+      </el-tab-pane>
+      <el-tab-pane label="Manage">
+        Manage
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-
+    data() {
+      return {
+        form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: '',
+          pass: '',
+          radio: '1'
+        }
+      }
+    },
+    methods: {
+      onSubmit() {
+        console.log('submit!');
+      }
+    }
   }
-}
 </script>
 
 <style scoped>
-
+  .el-dropdown-link {
+    cursor: pointer;
+    color: #409EFF;
+  }
+  .el-icon-arrow-down {
+    font-size: 12px;
+  }
+  .demonstration {
+    display: block;
+    color: #8492a6;
+    font-size: 14px;
+    margin-bottom: 20px;
+  }
 </style>
 
